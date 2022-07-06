b755c1410b57db98dc6850ee0caf5640d4f8ec27
Mateusz Słotwiński <78279510+mateusz-slotwinski@users.noreply.github.com>
2022-07-06 21:17:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Native
+Mobile version of Maxwell
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Maxwell',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: Scaffold(
+          appBar:
+              AppBar(title: const Center(child: Text('Welcome to Flutter!'))),
+          body: const Center(child: Text('Hello Flutter!'))),
+    );
+  }
+}
